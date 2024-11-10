eb66763b25aee67b5a0b53932ba4f0a737d04d7d
KFMBB <khh252548@gmail.com>
2024-11-10 21:40:06 +0300

    Job class implementation

--- src/Job.java
+++ src/Job.java
@@ -1,9 +1,19 @@
 public class Job {
-    private PCB pcb; // Each process (job) initialized will be assigned a PCB.
+    private PCB pcb ; // Each process (job) initialized will be assigned a PCB.
 
-    public Job(PCB pcb) {
-        this.pcb = pcb;
+    public Job(int jobId, int burstTime, int memoryRequired)
+    {
+        this.pcb = new PCB(jobId , burstTime , memoryRequired)  ;
     }
 
+    public PCB getPcb()
+    {
+        return pcb;
+    }
 
+    public String getJobDetails()
+    {
+        return "JobId : " + this.pcb.getId() + " Burst Time : " + this.pcb.getBurstTime() + " Memory Required : " + this.pcb.getMemoryRequired();;
+
+    }
 }
