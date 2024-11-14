4f1fbf803b84b5e4a71f430adbafd596ecb35e5f
O13 <Mromarxx5@gmail.com>
2024-11-14 19:00:16 +0300

    Adding roundrobin

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,6 +1,8 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+
+
 public class Scheduler implements Runnable {
     private Queue<Job> readyQueue;
     private final int timeQuantum = 8;
@@ -58,14 +60,47 @@ public class Scheduler implements Runnable {
     // Placholder for RR Algo
     public void runRoundRobin() {
 
-    }
+            int log = 0;
 
+            while (!readyQueue.isEmpty()){
+
+                Job job=readyQueue.poll();
+                int remainingTime = job.getPcb().getRemainingTime(); //RemainingTime inital is burst time
+                systemCall.startProcess(job);
+                int i=timeQuantum;
+                // Simulate job processing for TimeQuantum which is 8 ms
+                while(i>0 && remainingTime!=0) {
+                    remainingTime--;
+                    log++;
+                    i--;
+                }
+                // check if job is finshed if so terminate
+                if(remainingTime==0) {
+
+                    job.getPcb().setTurnaroundTime(log - job.getPcb().getArrivalTime());
+                    job.getPcb().setWaitingTime(job.getPcb().getTurnaroundTime() - job.getPcb().getBurstTime());
+                    systemCall.releaseMemory(job);
+                    systemCall.terminateProcess(job);
+                }
+                else {  //update remaingTime if not finshed and add to ready queue again until its finshed
+
+                    job.getPcb().setRemainingTime(remainingTime);
+                    readyQueue.add(job);
+
+                }
+
+            }
+
+
+
+    }
     // Placeholder for SJF ALGO
     public void runSJF() {
 
     }
 
     public void calculateStats() {
+
 
     }
 }
